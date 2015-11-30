df26a8d25e00cbb2ad99d85501c1f8f9345f8d83
idada <bg5sbk@gmail.com>
2015-12-01 00:31:23 +0800

    适配新版unitest

--- cmd_test.go
+++ cmd_test.go
@@ -7,27 +7,27 @@ import (
 
 func Test_Cmd(t *testing.T) {
 	Register("lookup api", "save api time record to file", func(args []string) interface{} {
-		unitest.Pass(t, args[0] == "lookup api")
+		unitest.AssertString(t, args[0], "lookup api")
 		return 1
 	})
 
 	Register("lookup gc", "save gc summary to file", func(args []string) interface{} {
-		unitest.Pass(t, args[0] == "lookup gc")
+		unitest.AssertString(t, args[0], "lookup gc")
 		return 2
 	})
 
 	Register("lookup heap ([0-2])", "save heap status to file", func(args []string) interface{} {
-		unitest.Pass(t, args[1] == "1")
+		unitest.AssertString(t, args[1], "1")
 		return 3
 	})
 
 	n, ok := Process("lookup api")
-	unitest.Pass(t, ok && n == 1)
+	unitest.Assert(t, ok && n == 1, n)
 	Process("lookup gc")
 
 	n, ok = Process("lookup gc")
-	unitest.Pass(t, ok && n == 2)
+	unitest.Assert(t, ok && n == 2, n)
 
 	n, ok = Process("lookup heap 1")
-	unitest.Pass(t, ok && n == 3)
+	unitest.Assert(t, ok && n == 3, n)
 }
