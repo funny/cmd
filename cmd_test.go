f89ddfa1d16acdbd210f40d7c39cfc28bc112931
dada <bg5sbk@gmail.com>
2015-12-01 12:58:45 +0800

    配合单元测试工具调整

--- cmd_test.go
+++ cmd_test.go
@@ -1,33 +1,33 @@
 package cmd
 
 import (
-	"github.com/funny/unitest"
+	"github.com/funny/utest"
 	"testing"
 )
 
 func Test_Cmd(t *testing.T) {
 	Register("lookup api", "save api time record to file", func(args []string) interface{} {
-		unitest.AssertString(t, args[0], "lookup api")
+		utest.EqualNow(t, args[0], "lookup api")
 		return 1
 	})
 
 	Register("lookup gc", "save gc summary to file", func(args []string) interface{} {
-		unitest.AssertString(t, args[0], "lookup gc")
+		utest.EqualNow(t, args[0], "lookup gc")
 		return 2
 	})
 
 	Register("lookup heap ([0-2])", "save heap status to file", func(args []string) interface{} {
-		unitest.AssertString(t, args[1], "1")
+		utest.EqualNow(t, args[1], "1")
 		return 3
 	})
 
 	n, ok := Process("lookup api")
-	unitest.Assert(t, ok && n == 1, n)
+	utest.Assert(t, ok && n == 1, n)
 	Process("lookup gc")
 
 	n, ok = Process("lookup gc")
-	unitest.Assert(t, ok && n == 2, n)
+	utest.Assert(t, ok && n == 2, n)
 
 	n, ok = Process("lookup heap 1")
-	unitest.Assert(t, ok && n == 3, n)
+	utest.Assert(t, ok && n == 3, n)
 }
